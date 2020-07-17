59280ab2706ce7387a08ab58c7ee206bdb94d4ac
netanelben <mtr574@gmail.com>
2020-07-17 15:56:16 +0300

    Add keywords

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Meter from '../src';
 
 import './style.css';
 
 storiesOf('Meter', module).add('Meter', () => {
-    const [value, setValue] = useState(50);
 
     const style = {
         maxWidth: '420px',
@@ -14,13 +13,24 @@ storiesOf('Meter', module).add('Meter', () => {
 
     return (
         <div style={style}>
-            <Meter value={value}
+            <Meter value={50}
                 title="Amount"
                 legend={["Lowest", "Mild", "Highest"]}
                 baseColor="rgba(102, 200, 55, 0.2)"
                 meterColor={{
                     outer: 'rgba(102, 204, 55, 0.3)',
                     inner: 'rgba(102, 204, 55, 1)'
+                }}/>
+
+            <br/><br/>
+
+            <Meter value={75}
+                title="Rating"
+                legend={["Okey", "Great", "Awesome!"]}
+                baseColor="rgba(102, 20, 55, 0.2)"
+                meterColor={{
+                    outer: 'rgba(102, 204, 55, 0)',
+                    inner: 'rgba(102, 20, 55, 1)'
                 }}/>
         </div>
     );
